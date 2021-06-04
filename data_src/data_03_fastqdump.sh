960686fbff5f21967bb35125f3bf667fe949f9f6
Jonathan David Crider <r2884@hpcwoods.olemiss.edu>
2021-06-04 11:49:57 -0500

    added the trimmomatic pbs script and updated the most recent imseq/flash settings I used

--- data_src/data_03_fastqdump.sh
+++
@@ -1,9 +0,0 @@
-
-#PROJDIR=
-
-#mkdir -p $PROJDIR/data/sra
-#cd $PROJDIR/data/sra
-
-version=2.11.0
-fastq-dump.${version} SRR392744 --split-files --gzip
-
--- data_src/step_002_fastqc_before_trimming.sh
+++
@@ -1,8 +0,0 @@
-#step_002_fastqc_before_trimming
-
-#commands
-mkdir -p /ddn/home2/r2884/VDJ_TCR/analysis/step_002_fastqc_before_trimming
-cd /ddn/home2/r2884/VDJ_TCR/analysis/step_001_fastq-dump
-qsub -I
-module load fastqc/0.11.9
-fastqc SRR392744_1.fastq.gz SRR392744_2.fastq.gz -o /ddn/home2/r2884/VDJ_TCR/analysis/step_002_fastqc
